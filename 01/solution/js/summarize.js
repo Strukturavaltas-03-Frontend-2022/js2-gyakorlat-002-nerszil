df1824902e1915f2acc01fddc6ae5e553bb70115
Szilárd Nérei <szilardnerei@gmail.com>
2022-08-21 21:26:03 +0200

    ma work

--- 01/solution/js/summarize.js
+++ 01/solution/js/summarize.js
@@ -1,7 +1,18 @@
+/*
+•	A 01/solution/js/summarize.js fájlban dolgozz!
+•	Készíts egy függvényt summarize néven!
+•	Ez a paraméterként kapott tetszőleges darabszámú, tetszőleges értékű egész számot összeadja, és visszatér az összeadás végeredményével.
+•	A paraméterként kapott értékek egyszerű number típusúak legyenek!
+•	Amennyiben bármelyik paraméter értéke vagy a részeredmény meghaladja a biztonságos tartományt, automatikusan konvertáljad BigInt-be, és természetesen a visszatérési érték is BigInt típusú legyen!
+*/
+
+
+function summarize () {
+    return a + b
+    if 
+}
 
 
 
 
-
-
-// export default summarize;
+export default summarize;
---
+++ 02/solution/js/numericConverter.js
@@ -0,0 +1,7 @@
+/*
+•	A 02/solution/js/numericConverter.js fájlban dolgozz!
+•	Készíts egy függvényt numericConverter néven!
+•	Ez a paraméterként megadott tízes számrendszerbeli számot átkonvertálja kettes, nyolcas és tizenhatos számrendszerbe is.
+•	Ezeket az értékeket egy objektumban add vissza. A property-k neve legyen: binary, octal, hexa.
+*/
+
